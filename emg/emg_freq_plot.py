[FIX] Use old gape calculation to match previous results

emg/emg_freq_plot.py
@@ -91,14 +91,15 @@ for plot_name, plot_data in zip(['gapes', 'ltps'], [gapes, ltps]):
         n_tastes = this_car['taste'].nunique()
         n_lasers = this_car['laser'].nunique()
         fig, ax = plt.subplots(
-                n_lasers, n_tastes,
+                n_tastes, n_lasers,
                 sharex = True, sharey = True,
-                figsize = (4*n_tastes, 4*n_lasers))
+                figsize = (4*n_lasers, 4*n_tastes))
         if n_tastes == 1:
             ax = np.expand_dims(ax, axis = 0)
         if n_lasers == 1:
             ax = np.expand_dims(ax, axis = 1)
-        for this_ind, this_dat, this_ax in zip(taste_laser_inds, taste_laser_data, ax.flatten()):
+        for this_ind, this_dat, this_ax in \
+                zip(taste_laser_inds, taste_laser_data, ax.flatten()):
             this_data_inds = this_dat.index.values
             this_plot_data = plot_data[this_data_inds]
             this_ax.set_title(f"Taste: {this_ind[0]}, Laser: {this_ind[1]}")
@@ -122,53 +123,56 @@ for plot_name, plot_data in zip(['gapes', 'ltps'], [gapes, ltps]):
         plt.close(fig)
 
 # Plot taste overlay per laser condition and CAR
-g = sns.relplot(
-        data = mean_emg_long,
-        x = 'time',
-        y = 'emg_value',
-        hue = 'taste',
-        row = 'laser',
-        col = 'car',
-        style = 'emg_type',
-        kind = 'line',
-        linewidth = 5,
-        alpha = 0.7,
-        )
-g.fig.suptitle('Taste Overlay')
-# Plot dashed line as x=0
-for ax in g.axes.flatten():
-    ax.axvline(0, color = 'red', linestyle = '--', linewidth = 2, alpha = 0.7)
-plt.tight_layout()
-g.savefig(
-        os.path.join(
-            plot_dir,
-            'taste_overlay.png'),
-        bbox_inches = 'tight')
-plt.close(g.fig)
+for car_name, car_dat in list(mean_emg_long.groupby('car')):
+    g = sns.relplot(
+            data = mean_emg_long,
+            x = 'time',
+            y = 'emg_value',
+            hue = 'taste',
+            row = 'laser',
+            col = 'emg_type',
+            # style = 'emg_type',
+            kind = 'line',
+            linewidth = 2,
+            # alpha = 0.7,
+            )
+    g.fig.suptitle('Taste Overlay')
+    # Plot dashed line as x=0
+    for ax in g.axes.flatten():
+        ax.axvline(0, color = 'red', linestyle = '--', linewidth = 2, alpha = 0.7)
+    sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+    plt.tight_layout()
+    g.savefig(
+            os.path.join(
+                plot_dir,
+                f'{car_name}_taste_overlay.png'),
+            bbox_inches = 'tight')
+    plt.close(g.fig)
 
 
-# Plot laser overlay per taste and CAR
-g = sns.relplot(
-        data = mean_emg_long,
-        x = 'time',
-        y = 'emg_value',
-        hue = 'laser',
-        row = 'car',
-        col = 'taste',
-        style = 'emg_type',
-        kind = 'line',
-        linewidth = 5,
-        alpha = 0.7,
-        )
-g.fig.suptitle('Laser Overlay')
-# Plot dashed line as x=0
-for ax in g.axes.flatten():
-    ax.axvline(0, color = 'red', linestyle = '--', linewidth = 2, alpha = 0.7)
-plt.tight_layout()
-g.savefig(
-        os.path.join(
-            plot_dir,
-            'laser_overlay.png'),
-        bbox_inches = 'tight')
-plt.close(g.fig)
+    # Plot laser overlay per taste and CAR
+    g = sns.relplot(
+            data = mean_emg_long,
+            x = 'time',
+            y = 'emg_value',
+            hue = 'laser',
+            row = 'emg_type',
+            col = 'taste',
+            # style = 'emg_type',
+            kind = 'line',
+            linewidth = 2,
+            # alpha = 0.7,
+            )
+    g.fig.suptitle('Laser Overlay')
+    # Plot dashed line as x=0
+    for ax in g.axes.flatten():
+        ax.axvline(0, color = 'red', linestyle = '--', linewidth = 2, alpha = 0.7)
+    sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+    plt.tight_layout()
+    g.savefig(
+            os.path.join(
+                plot_dir,
+                f'{car_name}_laser_overlay.png'),
+            bbox_inches = 'tight')
+    plt.close(g.fig)
 

emg/emg_freq_post_process.py
@@ -54,19 +54,19 @@ p_files = [os.path.join(results_path, f'trial{x:03}_p.npy') for x in trial_inds]
 omega_files = [os.path.join(results_path, f'trial{x:03}_omega.npy') for x in trial_inds]
 
 p_data = [np.load(x) for x in p_files]
-p_data = np.stack(p_data, axis = 0)
+p_data = np.stack(p_data, axis = 0) 
 omega_data = [np.load(x) for x in omega_files]
 # Get first non nan omega
 omega = [x for x in omega_data if not any(np.isnan(x))][0]
 
-# Convert p_data to 1d
-p_inds = np.where(p_data)
-p_flat = np.zeros(p_data.shape[:2])
-for this_iter in zip(*p_inds):
-    p_flat[this_iter[:2]] = omega[this_iter[-1]]
+# # Convert p_data to 1d
+# p_inds = np.where(p_data)
+# p_flat = np.zeros(p_data.shape[:2])
+# for this_iter in zip(*p_inds):
+#     p_flat[this_iter[:2]] = omega[this_iter[-1]]
 
 # Write out p_flat and omega to disk
-np.save(os.path.join('emg_output', 'p_flat.npy'), p_flat)
+# np.save(os.path.join('emg_output', 'p_flat.npy'), p_flat)
 np.save(os.path.join('emg_output', 'omega.npy'), omega)
 
 merge_frame = pd.merge(emg_trials_frame, trial_info_frame, 
@@ -74,8 +74,11 @@ merge_frame = pd.merge(emg_trials_frame, trial_info_frame,
                        right_on = ['dig_in_name_taste', 'taste_rel_trial_num'], 
                        how = 'left')
 merge_frame.drop(
-        columns = ['dig_in','trial_inds', 'start_taste','end_taste','start_laser','end_laser',
-                   'laser_duration','laser_lag','start_laser_ms','end_laser_ms',
+        columns = ['dig_in','trial_inds', 
+                   'start_taste','end_taste',
+                   'start_laser','end_laser',
+                   'laser_duration','laser_lag',
+                   'start_laser_ms','end_laser_ms',
                    'start_taste_ms','end_taste_ms',
                    ]
         , inplace = True)
@@ -88,13 +91,22 @@ merge_frame.to_csv('emg_output/emg_env_merge_df.csv')
 ############################################################
 # Segment by frequencies
 
-gape_array = np.logical_and(
-        p_flat >= 3,
-        p_flat <= 5
-        )
-ltp_flat = p_flat >= 5.5
+# gape_array = np.logical_and(
+#         p_flat >= 3,
+#         p_flat <= 5
+#         )
+# ltp_array = p_flat >= 5.5
+
+## Find the frequency with the maximum EMG power at each time point on each trial
+## Gapes are anything upto 4.6 Hz
+## LTPs are from 5.95 Hz to 8.65 Hz
+#Alternatively, gapes from 3.65-5.95 Hz (6-11). LTPs from 5.95 to 8.65 Hz (11-17) 
+gape_array = np.sum(p_data[:, :, 6:11], axis = 2)/\
+		np.sum(p_data, axis = 2)
+ltp_array = np.sum(p_data[:, :, 11:], axis = 2)/\
+		np.sum(p_data, axis = 2)
 
 # Write out
 np.save('emg_output/gape_array.npy', gape_array)
-np.save('emg_output/ltp_array.npy', ltp_flat)
+np.save('emg_output/ltp_array.npy', ltp_array)
 